de3bb1679e72002038e976b89ce7bd518bf3c8ed
Revanth9984 <revanthreddytarimala@gmail.com>
2024-12-16 19:48:17 +0530

    updated team page

--- src/pages/team/Teamiddesktop.js
+++ src/pages/team/Teamiddesktop.js
@@ -1,8 +1,17 @@
 import styles from "./Team.module.css";
-const Teamiddesktop = (img,name,position) => {
+import { FaLinkedin } from "react-icons/fa6";
+
+const Teamiddesktop = (img,name,position,linkedin) => {
     return (
         <div className={styles.Teamiddesktop}>
+
+            <div className={styles.linkedin}>
+            <a href={linkedin} target="_blank" rel="noopener noreferrer">
+            <div className={styles.icondiv}><FaLinkedin className={styles.icon}/></div>
             <img src={img}></img>
+            </a>
+            </div>
+            
             <div className={styles.imgTitleName}><p>{name}</p></div>
             <div className={styles.imgTitlePost}><p>{position}</p></div>
         </div>
--- src/pages/team/Teamidmobile.js
+++ src/pages/team/Teamidmobile.js
@@ -1,8 +1,17 @@
 import styles from "./Team.module.css";
-const Teamidmobile = (img,name,position) => {
+import { FaLinkedin } from "react-icons/fa6";
+
+const Teamidmobile = (img,name,position,linkedin) => {
     return (
         <div className={styles.Teamidmobile}>
+
+            <div className={styles.linkedin}>
+            <a href={linkedin} target="_blank" rel="noopener noreferrer">
+            <FaLinkedin className={styles.icon}/>
             <img src={img}></img>
+            </a>
+            </div>
+            
             <div className={styles.imgTitleName}><p>{name}</p></div>
             <div className={styles.imgTitlePost}><p>{position}</p></div>
         </div>
