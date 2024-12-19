ad896f43d60d08c8c128d1843934199f228bac92
Prashanth <prashanthrachamalla5905@gmail.com>
2024-12-19 13:45:48 +0530

    Added  Blogs page

--- src/pages/aboutUs/AboutUs.js
+++ src/pages/aboutUs/AboutUs.js
@@ -1,10 +1,15 @@
 import img1 from './pics/img1.jpg'
 import styles from './AboutUs.css'
 import AboutTed from '../../components/aboutus/AboutTed';
+import Blogs from '../../components/aboutus/Blogs'
 const AboutUs = () => {
-    return (
-     <AboutTed/>
+    return(
+      <>
+        <AboutTed/>
+        <Blogs/>
+      </>
+      
     );
-  }
+}
   
-  export default AboutUs;
+export default AboutUs;
