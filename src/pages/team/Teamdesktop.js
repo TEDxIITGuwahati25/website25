041d097bb7944bc6e21ffe4ed7d58eacfdfe4df8
meghananelapati <meghananelapti681@gmail.com>
2024-12-28 00:20:28 +0530

    fixed events page

--- src/pages/team/Teamdesktop.js
+++ src/pages/team/Teamdesktop.js
@@ -9,19 +9,18 @@ const Teamdesktop = () => {
                lives you can find us working in design, computer science, chemistry, biology, finance, NGOs, arts, business, psycology, health and 
                everything in between. We work year round to bring TEDxIITGuwahati and all of our activities to life.</p>
             <div className={styles.Teamdesktopdetails}>
-              {Teamiddesktop("images/Pranati_Tandon4.png","Pranati Tandon","Licensee","https://www.linkedin.com/in/pranati-tandon-a002b127a/")}
-              {Teamiddesktop("images/Devesh_Kamble.png","Devesh Kamble","Co-Licensee & Marketing Head","https://www.linkedin.com/in/devesh-kamble")}
-              {Teamiddesktop("images/Tanvi.png","Tanvi Doshi","Content Head","https://www.linkedin.com/in/tanvi-doshi-ba951b28b")}
-              {Teamiddesktop("images/Shubham2.png","Shubham Yadav","Content Core","https://www.linkedin.com/in/yadvshubham/")}
-              {Teamiddesktop("images/Meghana.png","Meghana Nelapati","WebOps Head","https://www.linkedin.com/in/meghana-nelapati-556b21261/")}
-              {Teamiddesktop("images/Dharani.png","Dharani Lunavath","Creatives Head","https://www.linkedin.com/in/dharani-lunavath-609b40233/")}
-              {Teamiddesktop("images/Navya2.png","Navya","Creatives Core","https://www.linkedin.com/in/navya-posham-530a58257/")}
-              {Teamiddesktop("images/Atharva3.png","Atharva Sant","Events Head","https://www.linkedin.com/in/atharva-sant-b88511295/")}
-              {Teamiddesktop("images/Naman2.png","Naman Sharma","Events Core","https://www.linkedin.com/in/naman-sharma-57734199/")}
-              {Teamiddesktop("images/Japleen2.png","Japleen Kaur","Speakers Head","https://www.linkedin.com/in/japleen-kaur-b10377260/")}
-              {Teamiddesktop("images/Anaswara2.png","Anaswara","Speaker Core","https://www.linkedin.com/in/anaswara-p-c-b99355141/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app")}
-              {Teamiddesktop("images/Divya2.png","Divya Sahiti","PR & Media Head","https://www.linkedin.com/in/divya-sahiti-yerramsetti-499a0924b/")}
-              {Teamiddesktop("images/Ayush2.png","Ayush Agarwal","PR & Media Core","https://www.linkedin.com/in/aayush-agarwal-iitg/")}
+              {Teamiddesktop("images/team/Pranati_Tandon4.png","Pranati Tandon","Licensee","https://www.linkedin.com/in/pranati-tandon-a002b127a/")}
+              {Teamiddesktop("images/team/Devesh_Kamble.png","Devesh Kamble","Co-Licensee & Marketing Head","https://www.linkedin.com/in/devesh-kamble")}
+              {Teamiddesktop("images/team/Tanvi.png","Tanvi Doshi","Content Head","https://www.linkedin.com/in/tanvi-doshi-ba951b28b")}
+              {Teamiddesktop("images/team/Shubham2.png","Shubham Yadav","Content Core","https://www.linkedin.com/in/yadvshubham/")}
+              {Teamiddesktop("images/team/Meghana.png","Meghana Nelapati","WebOps Head","https://www.linkedin.com/in/meghana-nelapati-556b21261/")}
+              {Teamiddesktop("images/team/Dharani.png","Dharani Lunavath","Creatives Head","https://www.linkedin.com/in/dharani-lunavath-609b40233/")}
+              {Teamiddesktop("images/team/Atharva3.png","Atharva Sant","Events Head","https://www.linkedin.com/in/atharva-sant-b88511295/")}
+              {Teamiddesktop("images/team/Naman2.png","Naman Sharma","Events Core","https://www.linkedin.com/in/naman-sharma-57734199/")}
+              {Teamiddesktop("images/team/Japleen2.png","Japleen Kaur","Speakers Head","https://www.linkedin.com/in/japleen-kaur-b10377260/")}
+              {Teamiddesktop("images/team/Anaswara2.png","Anaswara","Speaker Core","https://www.linkedin.com/in/anaswara-p-c-b99355141/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app")}
+              {Teamiddesktop("images/team/Divya2.png","Divya Sahiti","PR & Media Head","https://www.linkedin.com/in/divya-sahiti-yerramsetti-499a0924b/")}
+              {Teamiddesktop("images/team/Ayush2.png","Ayush Agarwal","PR & Media Core","https://www.linkedin.com/in/aayush-agarwal-iitg/")}
             </div>
         </div>
     );
