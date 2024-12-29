7b7893a3f57d88893b15f899ec223b0cba544fc4
meghananelapati <meghananelapti681@gmail.com>
2024-12-29 17:20:11 +0530

    added preloader

---
+++ src/components/preloader/Preloader.js
@@ -0,0 +1,20 @@
+import animationVideo from '../../animation/preloader.mp4';
+import './Preloader.css';
+
+const Preloader = ({ onVideoEnd }) => {
+    return (
+        <div className="preloader">
+            <video
+                className="preloader-video"
+                autoPlay
+                muted
+                onEnded={onVideoEnd} // Call the function when the video ends
+            >
+                <source src={animationVideo} type="video/mp4" />
+                Your browser does not support the video tag.
+            </video> 
+        </div>
+    );
+  }
+  
+  export default Preloader;
