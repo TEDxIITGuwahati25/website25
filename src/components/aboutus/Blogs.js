0bc54debdc6133a2da36a1a1713c0d693783f723
Prashanth <prashanthrachamalla5905@gmail.com>
2025-01-04 17:55:21 +0530

    updated blogs page

--- src/components/aboutus/Blogs.js
+++ src/components/aboutus/Blogs.js
@@ -1,4 +1,9 @@
-import p1 from './pics/img.jpg';
+import p1 from './pics/blog1_1.jpg';
+import p2 from './pics/blog2.jpg';
+import p3 from './pics/blog3.jpg';
+import p4 from './pics/blog4_1.jpg';
+import p5 from './pics/blog5_1.jpg';
+import p6 from './pics/blog6_1.jpg';
 import styles from "./Blogs.css";
 
 const Blogs = () => {
@@ -9,49 +14,50 @@ const Blogs = () => {
               </div>
               <div className='blogs'>
                     
-                    <div className='blog'>
+                    <a className='blog' href='https://tedxiitguwahati.medium.com/pursuing-the-elusive-calm-in-the-mysterious-labyrinth-of-life-23b85f328d16' target='_blank'>
                          <img src={p1} className='p1'></img>
-                         <h2 className='title'>Blog title</h2>
-                         <p className='blogInfo'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima in maiores impedit dolor perspiciatis, minus officiis quia fugiat, ea id assumenda. Saepe tempore minima perferendis voluptas voluptatibus laboriosam temporibus debitis.</p>
-                    </div>
+                         <p className='title'>Pursuing the Elusive Calm in the Mysterious Labyrinth of Life</p>
+                         <p className='blogInfo'>Prioritize mental well-being to foster peace and embrace life's journey.</p>
+                         
+                    </a>
                     
-                    <div className='blog'> 
-                         <img src={p1} className='p1'></img>
-                         <h2 className='title'>Blog title</h2>
-                         <p className='blogInfo'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima in maiores impedit dolor perspiciatis, minus officiis quia fugiat, ea id assumenda. Saepe tempore minima perferendis voluptas voluptatibus laboriosam temporibus debitis.</p>
-                    </div>
+                    <a className='blog' href='https://tedxiitguwahati.medium.com/the-curious-case-of-japans-vanishing-act-5b14977324c8' target='_blank'> 
+                         <img src={p2} className='p1'></img>
+                         <p className='title'>The Curious Case of Japan's Vanishing Act:</p>
+                         <p className='blogInfo'>Japan's deliberate disappearances, balancing reinvention and emotional fallout.</p>
+                    </a>
                     
-                    <div className='blog'>
+                    <a className='blog' href='https://tedxiitguwahati.medium.com/has-the-beautiful-game-lost-its-flair-ebc9a2b6bec1' target='_blank'>
 
-                         <img src={p1} className='p1'></img>
-                         <h2 className='title'>Blog title</h2>
-                         <p className='blogInfo'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima in maiores impedit dolor perspiciatis, minus officiis quia fugiat, ea id assumenda. Saepe tempore minima perferendis voluptas voluptatibus laboriosam temporibus debitis.</p>
+                         <img src={p3} className='p1'></img>
+                         <p className='title'>Has the 'Beautiful Game' Lost its flair?</p>
+                         <p className='blogInfo'>Football's shift: from artistry to athleticism, driven by profit and packed schedules.</p>
 
-                    </div>
+                    </a>
                     
                     
-                    <div className='blog'>
+                    <a className='blog' href='https://tedxiitguwahati.medium.com/gen-z-and-the-rise-of-influencer-culture-5d9d45f534a4' target='_blank'>
 
-                         <img src={p1} className='p1'></img>
-                         <h2 className='title'>Blog title</h2>
-                         <p className='blogInfo'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima in maiores impedit dolor perspiciatis, minus officiis quia fugiat, ea id assumenda. Saepe tempore minima perferendis voluptas voluptatibus laboriosam temporibus debitis.</p>
-                    </div>
+                         <img src={p4} className='p1'></img>
+                         <p className='title'>Gen Z And The Rise Of Influencer Culture</p>
+                         <p className='blogInfo'>Connecting Gen Z while fueling cyberbullying and mental health challenges.</p>
+                    </a>
                     
-                    <div className='blog'> 
+                    <a className='blog' href='https://tedxiitguwahati.medium.com/game-on-or-game-over-fa81214a4f47' target='_blank'> 
 
-                         <img src={p1} className='p1'></img>
-                         <h2 className='title'>Blog title</h2>
-                         <p className='blogInfo'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima in maiores impedit dolor perspiciatis, minus officiis quia fugiat, ea id assumenda. Saepe tempore minima perferendis voluptas voluptatibus laboriosam temporibus debitis.</p>
+                         <img src={p5} className='p1'></img>
+                         <p className='title'>Game On Or Game Over?</p>
+                         <p className='blogInfo'>Esports offers rewards but intense pressure; moderate gaming can boost well-being.</p>
 
-                    </div>
+                    </a>
                     
-                    <div className='blog'>
+                    <a className='blog' href='https://tedxiitguwahati.medium.com/do-aliens-exist-cd682f0dc7be' target='_blank'>
 
-                         <img src={p1} className='p1'></img>
-                         <h2 className='title'>Blog title</h2>
-                         <p className='blogInfo'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima in maiores impedit dolor perspiciatis, minus officiis quia fugiat, ea id assumenda. Saepe tempore minima perferendis voluptas voluptatibus laboriosam temporibus debitis.</p>
+                         <img src={p6} className='p1'></img>
+                         <p className='title'>Do Aliens Exist?</p>
+                         <p className='blogInfo'>This is a question everyone has posed at least once in their lives.</p>
 
-                    </div>
+                    </a>
                     
               </div>
           </>
