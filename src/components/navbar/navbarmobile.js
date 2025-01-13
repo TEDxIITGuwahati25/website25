074a661b6660c4229418522a2eb8d2c054228a19
meghananelapati <meghananelapti681@gmail.com>
2025-01-14 00:36:17 +0530

    added scroll to top on navbar and footer images

--- src/components/navbar/navbarmobile.js
+++ src/components/navbar/navbarmobile.js
@@ -4,6 +4,9 @@ import { TbXboxX } from "react-icons/tb";
 import {NavLink} from "react-router-dom";
 import { useState,useEffect,useRef} from "react";
 const Navbarmobile = () => {
+    const scrollToTop = () => {
+        window.scrollTo(0, 0);
+      };
     const showActivePage = (isActive,color)=>{return {color: isActive?color:''}}
     const [menu,setMenu] = useState(false)
     const togglemenu =()=>{
@@ -25,19 +28,19 @@ const Navbarmobile = () => {
     return (
         <>
                 <div className={styles.frustation} ref={menuRef}>
-                    <div className={styles.navtop}><img src="./images/WhiteTextLogo.png"></img></div>
+                    <div className={styles.navtop}><NavLink to="/" onClick={scrollToTop}><img src="./images/WhiteTextLogo.png" alt="Home Logo" /></NavLink></div>
                     {menu ===true ? <TbXboxX className={styles.menubutton} onClick={togglemenu}/> : <CiMenuBurger className={styles.menubutton} onClick={togglemenu}/>}
                     {/* <button className={styles.menubutton} onClick={togglemenu}></button> */}
                 </div> 
 
         <div className={menu ===true ? styles.navbarmobileopen : styles.navbarmobile} ref={menuRef} onClick={togglemenu}>
             {menu && <div className={styles.div}>
-                <div className={styles.div1}><NavLink to="/" style={({isActive})=>showActivePage(isActive,'red')}>Home</NavLink></div>
-                <div className={styles.div2}><NavLink to="/AboutUs" style={({isActive})=>showActivePage(isActive,'red')}>About Us</NavLink></div>
-                <div className={styles.div3}><NavLink to="/Team" style={({isActive})=>showActivePage(isActive,'red')}>Team</NavLink></div>
-                <div className={styles.div4}><NavLink to="/Events" style={({isActive})=>showActivePage(isActive,'red')}>Events</NavLink></div>
-                <div className={styles.div5}><NavLink to="/ContactUs" style={({isActive})=>showActivePage(isActive,'red')}>Contact Us</NavLink></div>
-                <div className={styles.div6}><NavLink to="/buyTickets" style={({isActive})=>showActivePage(isActive,'red')}>Book Now</NavLink></div>
+                <div className={styles.div1}><NavLink to="/" onClick={scrollToTop}style={({isActive})=>showActivePage(isActive,'red')}>Home</NavLink></div>
+                <div className={styles.div2}><NavLink to="/AboutUs" onClick={scrollToTop} style={({isActive})=>showActivePage(isActive,'red')}>About Us</NavLink></div>
+                <div className={styles.div3}><NavLink to="/Team"onClick={scrollToTop} style={({isActive})=>showActivePage(isActive,'red')}>Team</NavLink></div>
+                <div className={styles.div4}><NavLink to="/Events"onClick={scrollToTop} style={({isActive})=>showActivePage(isActive,'red')}>Events</NavLink></div>
+                <div className={styles.div5}><NavLink to="/ContactUs"onClick={scrollToTop} style={({isActive})=>showActivePage(isActive,'red')}>Contact Us</NavLink></div>
+                <div className={styles.div6}><NavLink to="/buyTickets" onClick={scrollToTop} style={({isActive})=>showActivePage(isActive,'red')}>Book Now</NavLink></div>
                 
             </div>}
         </div>
